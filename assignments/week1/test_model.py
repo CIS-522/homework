0198535aab4ee8b58f04dfcc24520d8d18ff5a80
Milton Pividori <miltondp@gmail.com>
2023-01-29 23:01:48 -0500

    week1: restore original unit tests

--- assignments/week1/test_model.py
+++ assignments/week1/test_model.py
@@ -18,62 +18,6 @@ model_parametrize = pytest.mark.parametrize(
 
 
 @model_parametrize
-def test_model_predict_without_fit(model):
-    """
-    Test that the model raises an error if it is not fit before predicting.
-    """
-    lr = model()
-    X = np.array([[1, 2], [3, 4]])
-    try:
-        lr.predict(X)
-        pytest.fail("Model did not raise an error when not fit.")
-    except ValueError as e:
-        assert str(e) == "Model has not been fit yet."
-
-
-@model_parametrize
-def test_small_linear_and_noisy_data(model):
-    """
-    Test that the model can fit simple linear data and noisy data.
-    """
-    np.random.seed(0)
-
-    # linear data, example from sklearn.LinearRegression
-    X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
-    # y = 1 * x_0 + 2 * x_1 + 3
-    y = np.dot(X, np.array([1, 2])) + 3
-    lr = model()
-    lr.fit(X, y)
-    y_hat = lr.predict(X)
-    np.testing.assert_allclose(y_hat, y, rtol=1e-3)
-
-    # noisy data
-    X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
-    # y = 1 * x_0 + 2 * x_1 + 3
-    y = np.dot(X, np.array([1, 2])) + 3
-    X = X + np.random.normal(0, 0.1, size=X.shape)
-    lr = model()
-    lr.fit(X, y)
-    y_hat = lr.predict(X)
-    np.testing.assert_allclose(y_hat, y, rtol=1e-2)
-
-
-@model_parametrize
-def test_fit_returns_self(model):
-    """
-    Test that the fit method returns self.
-    """
-    np.random.seed(0)
-
-    # linear data, example from sklearn.LinearRegression
-    X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
-    # y = 1 * x_0 + 2 * x_1 + 3
-    y = np.dot(X, np.array([1, 2])) + 3
-    lr = model()
-    lr.fit(X, y).predict(X)
-
-
-@model_parametrize
 def test_has_correct_attributes(model):
     """
     Test that the LinearRegression class has the correct attributes.
