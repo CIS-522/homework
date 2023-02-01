20335f11af3778065d3e008b35fcab1d7eb8cf05
程果 <guo@hodokas-MacBook-Pro.local>
2023-01-31 20:58:15 -0500

    hw1

--- assignments/week1/model.py
+++ assignments/week1/model.py
@@ -3,17 +3,22 @@ import numpy as np
 
 class LinearRegression:
 
-    w: np.ndarray
-    b: float
+    # w: np.ndarray
+    # b: float
 
     def __init__(self):
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        self.w = None
+        self.b = None
 
     def fit(self, X, y):
-        raise NotImplementedError()
+        self.w = np.linalg.inv(X.T @ X) @ X.T @ y
+        self.b = y.mean() - self.w @ X.mean(axis = 0)
+        # raise NotImplementedError()
 
     def predict(self, X):
-        raise NotImplementedError()
+        return X.dot(self.w) + self.b
+        # raise NotImplementedError()
 
 
 class GradientDescentLinearRegression(LinearRegression):
@@ -23,18 +28,35 @@ class GradientDescentLinearRegression(LinearRegression):
 
     def fit(
         self, X: np.ndarray, y: np.ndarray, lr: float = 0.01, epochs: int = 1000
-    ) -> None:
-        raise NotImplementedError()
+    ) -> None :
 
-    def predict(self, X: np.ndarray) -> np.ndarray:
-        """
+        self.m, self.n = X.shape
+        self.w = np.zeros(self.n,1)
+        self.b = 0
+        for i in range(epochs):
+
+            y_pred = self.predict(X)
+            #calculate gradient
+            
+            d_w = -(2*(X.T)@(y-y_pred))/self.m
+            d_b = -2*np.sum(y-y_pred)/self.m
+
+            #update params
+            self.w -= lr * d_w
+            self.b -= lr * d_b 
+
+
+    """
         Predict the output for the given input.
-
         Arguments:
             X (np.ndarray): The input data.
-
         Returns:
             np.ndarray: The predicted output.
+    """
+    def predict(self, X: np.ndarray) -> np.ndarray: 
+        
+        return X @ self.w + self.b
+        
 
-        """
-        raise NotImplementedError()
+
+       
